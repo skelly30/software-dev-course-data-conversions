c311d09a1e2c8a7083a483fd414421da5e354578
Summer Kelly <72564993+skelly30@users.noreply.github.com>
2025-09-26 19:55:46 -0500

    Update explicit-and-implicit-conversion-in-javascript.js
    fixed bugs, added implicit and explicit examples

--- explicit-and-implicit-conversion-in-javascript.js
+++ explicit-and-implicit-conversion-in-javascript.js
@@ -22,11 +22,20 @@ Use console.log() to clearly show the before-and-after type conversions.
 let result = "5" - 2;
 console.log("The result is: " + result);
 
-let isValid = Boolean("false");
+let someString = "false";
+let isValid = (someString === "true");
 if (isValid) {
     console.log("This is valid!");
 }
 
 let age = "25";
-let totalAge = age + 5;
+let totalAge = Number(age) + 5;
 console.log("Total Age: " + totalAge);
+
+// my explicit example
+console.log(String(25));
+
+// my implicit example
+let sentence = "The answer to this question is " + true;
+console.log(sentence)
+
